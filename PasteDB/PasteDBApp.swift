00da2e2c2eaac00e495d453bf817fc23b6fccf8b
Raheem Lawal <lawalraheem@gmail.com>
2024-01-04 17:17:46 +0100

    Initial Commit

---
+++ PasteDB/PasteDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasteDBApp.swift
+//  PasteDB
+//
+//  Created by Raheem Lawal on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PasteDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
